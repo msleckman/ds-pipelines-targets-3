c14b0a8b228c6387c38235f2eda16edc709ade29
Margaux Sleckman <msleckman@usgs.gov>
2021-12-17 15:34:53 -0800

    also pushing minor comment edits to tally_site-obs.R

--- 2_process/src/tally_site_obs.R
+++ 2_process/src/tally_site_obs.R
@@ -2,10 +2,13 @@
 # packages: lubridate, tidyverse
 tally_site_obs <- function(site_data) {
   message(sprintf('  Tallying data for %s-%s', site_data$State[1], site_data$Site[1]))
+
   site_data %>%
-    mutate(Year = year(Date)) %>%
+  ## SPLIT
+      mutate(Year = year(Date)) %>%
     # group by Site and State just to retain those columns, since we're already
     # only looking at just one site worth of data
     group_by(Site, State, Year) %>%
+  # Apply (computation in length(which(!is.na()))) & COMBINE (summarize)
     summarize(NumObs = length(which(!is.na(Value))), .groups = "keep")
 }
