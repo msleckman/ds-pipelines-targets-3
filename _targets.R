3c07ba45dd31a8cb1849d690e94ebc3a959d0269
Margaux Sleckman <msleckman@usgs.gov>
2021-12-23 19:40:11 -0800

    adding retry() to tar_make()"

--- _targets.R
+++ _targets.R
@@ -2,6 +2,7 @@
 library(targets)
 library(tarchetypes)
 library(tibble)
+library(retry)
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(dplyr))
 
@@ -32,7 +33,11 @@ source('3_visualize/src/map_timeseries.R')
 dir.create('3_visualize/log/', showWarnings = F)
 
 ## Configuration - define global vars
-states <- c('WI','MN','MI','IL', 'IN', 'IA')
+states <- c('AL','AZ','AR','CA','CO','CT','DE','DC','FL','GA','ID','IL','IN','IA',
+            'KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+            'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX',
+            'UT','VT','VA','WA','WV','WI','WY','AK','HI','GU','PR')
+
 parameter <- c('00060')
 
 ## Structure static branching Targets with tar_map()
@@ -50,9 +55,10 @@ mapped_by_state_targets <- tar_map(
   tar_target(nwis_inventory, dplyr::filter(oldest_active_sites,
                                            state_cd == state_abb)),
   #Target 2 - Download data for given site
-  tar_target(nwis_data, get_site_data(nwis_inventory,
+  tar_target(nwis_data, retry::retry(get_site_data(nwis_inventory,
                                       state_abb,
-                                      parameter)),
+                                      parameter), when = "Ugh, the internet data transfer failed!",
+                                     max_tries = 30)),
   # Target 3 - Clean and summarize data for each state
   tar_target(tally, tally_site_obs(nwis_data)),
 
